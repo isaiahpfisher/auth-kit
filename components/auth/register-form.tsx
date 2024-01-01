04464a416942eaf009fd5e45d91b4b38a32c5e37
isaiahpfisher <isaiahpfisher@gmail.com>
2024-01-01 10:09:42 -0600

    create user (register)

--- components/auth/register-form.tsx
+++ components/auth/register-form.tsx
@@ -1,6 +1,12 @@
 "use client";
 
-import { CardWrapper } from "@/components/auth/card-wrapper";
+import * as z from "zod";
+import { useState, useTransition } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { RegisterSchema } from "@/schemas";
+import { Input } from "@/components/ui/input";
 import {
   Form,
   FormControl,
@@ -9,21 +15,16 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-import { RegisterSchema } from "@/schemas";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import * as z from "zod";
-import { Input } from "@/components/ui/input";
+import { CardWrapper } from "@/components/auth/card-wrapper";
 import { Button } from "@/components/ui/button";
 import { FormError } from "@/components/form-error";
 import { FormSuccess } from "@/components/form-success";
 import { register } from "@/actions/register";
-import { useState, useTransition } from "react";
 
 export const RegisterForm = () => {
-  const [isPending, startTransition] = useTransition();
   const [error, setError] = useState<string | undefined>("");
   const [success, setSuccess] = useState<string | undefined>("");
+  const [isPending, startTransition] = useTransition();
 
   const form = useForm<z.infer<typeof RegisterSchema>>({
     resolver: zodResolver(RegisterSchema),
@@ -67,8 +68,6 @@ export const RegisterForm = () => {
                       {...field}
                       disabled={isPending}
                       placeholder="John Doe"
-                      type="text"
-                      autoComplete="off"
                     />
                   </FormControl>
                   <FormMessage />
@@ -87,7 +86,6 @@ export const RegisterForm = () => {
                       disabled={isPending}
                       placeholder="john.doe@example.com"
                       type="email"
-                      autoComplete="off"
                     />
                   </FormControl>
                   <FormMessage />
@@ -106,7 +104,6 @@ export const RegisterForm = () => {
                       disabled={isPending}
                       placeholder="••••••••"
                       type="password"
-                      autoComplete="off"
                     />
                   </FormControl>
                   <FormMessage />
@@ -116,8 +113,8 @@ export const RegisterForm = () => {
           </div>
           <FormError message={error} />
           <FormSuccess message={success} />
-          <Button type="submit" className="w-full" disabled={isPending}>
-            Create an Account
+          <Button disabled={isPending} type="submit" className="w-full">
+            Create an account
           </Button>
         </form>
       </Form>
