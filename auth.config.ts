95a99ac27a18e38f80a871e8f56fe808f59e7756
isaiahpfisher <isaiahpfisher@gmail.com>
2024-01-01 12:12:15 -0600

    callbacks

--- auth.config.ts
+++ auth.config.ts
@@ -2,7 +2,7 @@ import type { NextAuthConfig } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import { LoginSchema } from "@/schemas";
 import { getUserByEmail } from "@/data/user";
-import bcrpyt from "bcryptjs";
+import bcrypt from "bcryptjs";
 
 export default {
   providers: [
@@ -16,7 +16,7 @@ export default {
           const user = await getUserByEmail(email);
           if (!user || !user.password) return null;
 
-          const passwordsMatch = await bcrpyt.compare(password, user.password);
+          const passwordsMatch = await bcrypt.compare(password, user.password);
 
           if (passwordsMatch) return user;
         }
